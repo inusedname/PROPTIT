2abeeb5feec50ca49d2c32f0bff6f64c241a30bc
inusedname <49682088+inusedname@users.noreply.github.com>
2020-12-17 14:09:23 +0700

    Create buoi3_baitap6.c

---
+++ buoi3/buoi3_baitap6.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <math.h>
+int main ()
+{
+    int n;
+    int nga = n-1;
+    int sao = 1;
+    scanf("%d",&n);
+    for (int i=1; i<2*n; i++)
+    {
+        for (int j=1; j<=abs(nga); j++)
+            printf("~");
+        for (int q=1; q<=sao; q++)
+            printf("*");
+        nga--;
+        if(i<n)
+            sao=sao+2;
+        else sao=sao-2;
+    }
+}
