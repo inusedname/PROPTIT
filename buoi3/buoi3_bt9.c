57da04ed06c7fa2d7e428e4a028365231e0c6ea8
inusedname <49682088+inusedname@users.noreply.github.com>
2020-12-18 08:15:13 +0700

    Create buoi3_bt9.c

---
+++ buoi3/buoi3_bt9.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+    long long n,sum=0;
+    int du;
+    scanf("%d", &n);
+    for(int i=n; i!=0; i/10)
+    {
+        du = i%10;
+        sum = sum + du; 
+    }
+    printf("%d",sum);
+}
